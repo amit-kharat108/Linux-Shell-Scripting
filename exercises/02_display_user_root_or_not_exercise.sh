e6f229c6bd02b473c3b0624c20570452ff6c6bf3
Amit Kharat <akharat@vishnu.localdomain>
2025-07-04 02:13:59 +0530

    02_display_user_root_or_not_exercise.sh

---
+++ exercises/02_display_user_root_or_not_exercise.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Display the UID and the username of the user executing this script
+# Display if the user is root or not
+
+# Display the UID
+
+
+
+# Display the username
+
+
+
+#Check if the user executing this script is root or not
