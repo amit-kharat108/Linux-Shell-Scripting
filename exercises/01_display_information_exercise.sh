c00a372fcbf7e6b42ff39d3ded5abab14f429088
Amit Kharat <akharat@vishnu.localdomain>
2025-07-04 01:29:50 +0530

    01_display_information_exercise.sh

---
+++ exercises/01_display_information_exercise.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This script displays various information to the screen
+
+# Display "Hello" to the screen
+
+
+
+# Assigning a value to variable WORD="script"
+
+
+
+# Display the value using that variable
+
+
+
+# Demonstrate the single quotes does not print the variables
+
+
+
+# Combine a variable with hard coded text
+
+
+
+# Display the contents of the variable using different syntax
+
+
+
+# Append a text to variable
+
+
+
+# Show how not to append a text to a variable
+# This is not how you append the text:
+
+
+
+# Create the a variable ENDING='ed'
+
+
+
+# Combine two variables
+
+
+
+# Change the value of ENDING variable
+
+
+
+# Combine the variables again
+
+
+
+# Re-assign the value of ENDING value
+
+
+
+# Combing the variable again
