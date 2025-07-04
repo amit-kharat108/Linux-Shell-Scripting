21fcd433e1792ee199f297dc6979738a8bb7e2b2
Amit Kharat <akharat@vishnu.localdomain>
2025-07-05 03:15:38 +0530

    03_use_of_exit_statuses.sh

---
+++ exercises/03_use_of_exit_statuses.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Display the UID and the username of the user executing the script
+# Display the user is akharat or not
+
+# Display the UID
+
+
+# Only Display the UID if this matches to 1000
+
+
+# Display the username
+
+
+
+# Test if the command succeeded
+
+
+
+# You can use a string test conditional (set ready user name in form of string to check against id -un)
+
+
+
+# Test for !=(not equal) for the string 
