059f37d65db6c81835a10c4ec5d47be508bbb95c
Amit Kharat <akharat@vishnu.localdomain>
2025-07-05 04:34:20 +0530

    04_read_the_user_input.sh

---
+++ exercises/04_read_the_user_input.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script creates an account on the local system
+# You will be prompted for an account name and the password
+
+# Ask for the username (username should always be 8 chars and not more than that)
+
+
+
+# Ask for the user's full name
+
+
+
+# Ask for the password
+
+
+
+# Create the user
+
+
+
+# Set the password for the user
+
+
+
+# Force the password change on the first login
+
+
