37eec5ab38f4cd71ae186b6ea54e48b41eb88254
Amit Kharat <akharat@vishnu.localdomain>
2025-07-06 20:37:27 +0530

    exercises

---
+++ 07_arguments_parameters_exercise.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script generates a random password for each user specified on the command line
+
+# Display what the user type on the command line
+
+
+# Display the path and the filename of the script
+
+
+# Tell how many arguments they passed while executing the script
+# Inside the script they are parameters, outside of the script they are arguments
+
+# Make sure they supply at least 1 argument
+
+
+# Generate and display a password for each parameter (user)
+
