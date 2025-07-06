53baeab44d1d6d9b06329e62a0467ba76fa7dd12
Amit Kharat <akharat@vishnu.localdomain>
2025-07-06 17:08:43 +0530

    06_generate_random_passwords_exercise.sh

---
+++ exercises/06_generate_random_passwords_exercise.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script generates a list of random passwords
+
+# A random number as a password
+
+
+
+# Three random numbers as a password
+
+
+
+# Use current date/time as the basis for the password(use seconds and nano seconds)
+
+
+
+# Create a better password with sha256sum and the date
+
+
+
+# Create a better password with a special character
