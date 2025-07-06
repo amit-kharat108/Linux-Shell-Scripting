34b7bc89dbd9a854ad0e2c8b4b3d1541563a8de8
Amit Kharat <akharat@vishnu.localdomain>
2025-07-06 22:56:25 +0530

    exercises

---
+++ 09_exercise_number_2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Enforce user to run the script with sudo or root
+
+
+
+# Provide user the help if they do not give the username for the account creation.
+
+
+    # Capture the username and comment(full_name) from the command line
+    # Use shift to capture the comment
+   
+
+    # Generate the password for the new user account
+
+    
+    # Create the user
+
+    
+
+    # Set the password for the user
+
+    
+
+    # Set password expiry
+
+    
+
+    # Print everything to user
+
+    
