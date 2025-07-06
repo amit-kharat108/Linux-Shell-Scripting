9923efc66ad7be90d3a8cea3423d75488730a13d
Amit Kharat <akharat@vishnu.localdomain>
2025-07-07 01:31:01 +0530

    exercises

---
+++ 10_IO_redirection_exercise.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# This script demonstrates I/O redirection
+
+# Redirect STDOUT to a file
+
+
+
+# Redirect STDIN to a file
+
+
+# Redirect the STDOUT to a file, overwrite that file
+
+
+# Redirect STDOUT to a file, append to that file
+
+
+
+
+# Redirect STDIN to a file, using FD 0
+
+
+
+# Redirect STDOUT to a file, using FD 1
+
+
+
+# Redirect STDERR to a file, using FD 2
+
+
+
+# Redirect STDOUT and STDERR to a file
+
+
+# Redirect STDOUT and STDERR through a pipe
+
+
+
+# Discard STDOUT
+
+
+
+# Discar STDIN
+
+# Discard both STDOUT and STDERR
