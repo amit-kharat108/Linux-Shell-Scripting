b6c497f911e8b3b71fbacca3e37326e45f77b045
Amit Kharat <akharat@vishnu.localdomain>
2025-07-06 21:26:23 +0530

    exercises

---
+++ 08_use_of_shift_and_while_loops_exercise.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Demonstrate the use of shift and while loops
+
+# Display the first 3 parameters which user will provide
+
+
+# Loop through all the positional parameters(utilizing while and shift)
