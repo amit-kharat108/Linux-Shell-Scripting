464f6f4c40a50a48f85407a3b8cbb830c7d6ff11
Amit Kharat <akharat@vishnu.localdomain>
2025-07-07 16:38:54 +0530

    exercises

---
+++ 11_exercise_number_3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#Make sure the scripts is being executed as the root user or not.
+
+
+#Make sure user provides at least one argument
+
+
+#The first parameter is the username
+
+
+#Rest of the parameters are the comments
+
+
+#Generate the password
+
+
+#Create the user account with password also don't show this output to the user
+
+
+#Check to see if the user account command succeded
+
+
+#Set the password
+
+
+#Check to see if the passwd command succeded
+
+
+#Force the password change on the first login
+
+
+#Print all the information to the user
+
+
