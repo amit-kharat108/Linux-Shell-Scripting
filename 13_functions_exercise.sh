151a619de7304bd3b67666c16a63bb180bb2f835
Amit Kharat <akharat@vishnu.localdomain>
2025-07-07 20:17:45 +0530

    exercises

---
+++ 13_functions_exercise.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Write a function which writes log to syslog when the VERBOSE is true.
+
+
+# Create a function which will take a backup of the file and will return a non-zero exit code on error.
+
+
+
+# Make decisions based on the status code of the functions
+
