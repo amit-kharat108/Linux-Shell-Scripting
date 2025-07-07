071d6bde9a809dcececeea532bca08f7dc14b74b
Amit Kharat <akharat@vishnu.localdomain>
2025-07-07 18:35:29 +0530

    exercises

---
+++ 12_the_case_statement_exercise.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script demonstrate the case statement
+# Make use of case "variable" in to handle the if-elif in a better way
+# Use the cases like start, stop, status and handle the other inputs
+
+##  Method 1 of using the case statement
+
+
+# The below chunk of the code works as well (Method 2 without spaces)
