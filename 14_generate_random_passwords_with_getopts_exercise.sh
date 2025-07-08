e0108efc275b06e08c2d05899368af3274e1602b
Amit Kharat <akharat@vishnu.localdomain>
2025-07-09 03:22:36 +0530

    exercises

---
+++ 14_generate_random_passwords_with_getopts_exercise.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script generates a random password
+# This user can set the password length with -l option and can add a special character with -s
+# Verbose mode can be enabled with -v
+
+# Define the usage function
+
+
+# Define the log function for verbosity
+
+# Set the default password length
+
+
+
+# getopts with a while loop
+
+# Generate the password with special char if user asks for it
+
+# Display the password
+
