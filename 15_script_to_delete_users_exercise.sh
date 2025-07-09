604222bf32ad99e62fe31c79e76d82aff92f23d6
Amit Kharat <akharat@vishnu.localdomain>
2025-07-10 01:41:30 +0530

    exercises

---
+++ 15_script_to_delete_users_exercise.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This scritpt deletes the users in linux system
+
+# Run this script as root
+
+
+# Assume the first argument of the script to be the user that we are going to delete
+
+
+# Delete the user
+
+
+# Make sure the user account got deleted
+
+
+# Tell the user that the account was deleted
+
+
