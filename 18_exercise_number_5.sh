88e72ce233a13d395675f8d255651710bfd461c2
Amit Kharat <akharat@vishnu.localdomain>
2025-07-12 00:33:38 +0530

    exercise

---
+++ 18_exercise_number_5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Count the number of failed logins by IP address.
+# If there are any IP with over LIMIT failures, display the count, IP and the location
+
+
+# Make sure file was supplied as an argument
+
+# Loop through the failed attempts and the correspondin IP address
+
